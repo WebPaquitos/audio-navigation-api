c8200b32209161495bdad862314852d915706f0b
David Alecrim <david.alecrim1@gmail.com>
2017-09-29 14:58:23 +0100

    changes to route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!')
+router.get('/api/ai', (req, res) => {
+    res.send('Hello World!');
 });
 
 module.exports = router;
