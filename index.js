6b76fb6d7dac32e2700bc2221e3aa7839ed395ee
Luis <luis.fgbo@gmail.com>
2017-09-29 14:43:49 +0100

    initial setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const routes = require('./routes/index');
+const bodyParser = require('body-parser');
+
+const express = require('express');
+const app = express();
+
+// Takes the raw requests and turns them into usable properties on req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// After allllll that above middleware, we finally handle our own routes!
+app.use('/', routes);
+
+// Start our app!
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+});
