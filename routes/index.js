85fec6e1a0537ad6f25bb147f91a105081fce39e
David Alecrim <david.alecrim1@gmail.com>
2017-09-29 15:44:59 +0100

    added api ai logic to server

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const ApiAiClient = require('apiai');
 const router = express.Router();
+const AiClient = new ApiAiClient('8fd7835cb9ea4a97849eb376652e3e4e');
 
-router.get('/api/ai', (req, res) => {
-    res.send('Hello World!');
+router.post('/api/ai/request', (req, res) => {
+    console.log(req.body);
+    const text = req.body.text;
+    AiClient.textRequest(text).then(({ result }) => {
+        console.log(result);
+        const { action, parameters } = result;
+        let whereto = '';
+        if (parameters) whereto = parameters.whereto;
+        else return res.status(400).json({ error: 'say_target' });
+        if (action === 'navigate') res.json({ target: whereto });
+    }).catch(() => {
+        console.log('error');
+        res.status(400).json({ error: 'repeat_command' });
+    });
 });
 
 module.exports = router;
