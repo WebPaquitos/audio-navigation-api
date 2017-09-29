aac3397385979d52b389b364489371501748ae7f
David Alecrim <david.alecrim1@gmail.com>
2017-09-29 16:27:35 +0100

    proxy is now successfully contacting API.ai

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const ApiAiClient = require('apiai');
 const router = express.Router();
-const AiClient = new ApiAiClient('8fd7835cb9ea4a97849eb376652e3e4e');
+const CLIENT_ACCESS_TOKEN = '8fd7835cb9ea4a97849eb376652e3e4e';
+const AiClient = new ApiAiClient(CLIENT_ACCESS_TOKEN);
 
 router.post('/api/request', (req, res) => {
-    console.log(req.body);
     const text = req.body.text;
-    AiClient.textRequest(text).then(({ result }) => {
-        console.log(result);
+    const request = AiClient.textRequest(text, {
+        sessionId: `${new Date().getTime()}`,
+    });
+
+    request.on('response', ({ result }) => {
         const { action, parameters } = result;
         let whereto = '';
         if (parameters) whereto = parameters.whereto;
         else return res.status(400).json({ error: 'say_target' });
         if (action === 'navigate') res.json({ target: whereto });
-    }).catch(() => {
-        console.log('error');
-        res.status(400).json({ error: 'repeat_command' });
     });
+
+    request.on('error', error => res.status(400).json({ error: 'repeat_command' }));
+
+    request.end();
 });
 
 module.exports = router;
